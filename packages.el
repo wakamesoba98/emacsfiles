f09ef63fca52852139377f57734bf220cfe7085c
wakamesoba98 <wakamesoba98@gmail.com>
2019-04-11 16:09:23 +0900

    Use melpa-stable

--- packages.el
+++ packages.el
@@ -1,5 +1,5 @@
 (defvar repositories
-  '(("melpa"     . "http://melpa.org/packages/")
+  '(("melpa"     . "http://stable.melpa.org/packages/")
     ("marmalade" . "http://marmalade-repo.org/packages/")
     ("gnu"       . "http://elpa.gnu.org/packages/")))
 
